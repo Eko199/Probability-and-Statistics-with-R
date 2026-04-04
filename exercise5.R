8e7d6b837142a31ec7d08bc7c019c2adc317fb27
Viktor Pazhev <asusvikp@gmail.com>
2026-04-04 15:47:26 +0300

    Finish exercise 5
    Add solutions and exploratory code for Problems 46–59: exponential simulations and transforms, inverse-transform sampling from uniform to exponential, normal tail probabilities/quantiles, synthetic vectors (v1–v5) with hist/mean/sd, additional survey plots and summaries, barplots for exercise frequency and height bins, and a redefined my.summary helper (returns median, mean, sd). These changes add analysis/plotting snippets throughout exercise5.R for the new problem blocks.

--- exercise5.R
+++ exercise5.R
@@ -1,6 +1,6 @@
 # Problem 44
 # piR^2/(4R^2) = pi / 4
-n <- 10^7
+n <- 10^4
 x <- runif(n, -1, 1)
 y <- runif(n, -1, 1)
 
@@ -11,7 +11,6 @@ curve(sqrt(1-x^2), from=-1, to=1, add=T, col="red")
 curve(-sqrt(1-x^2), from=-1, to=1, add=T, col="red")
 
 # Problem 45
-# piR^2/(4R^2) = pi / 4
 n <- 10^7
 
 f <- function(x) {
@@ -28,7 +27,26 @@ curve(f(x), from=0.8, to=4, add=T, col="red")
 
 pnorm(4) - pnorm(0.8)
 
-#...
+# Problem 46
+x <- rexp(5000, 1/8)
+hist(x)
+
+y <- 1 - exp(-x/8)
+hist(y)
+
+# Problem 47
+x <- runif(5000, 0, 1)
+hist(x)
+
+y <- (-1/(1/8)) * log(1 - x)
+hist(y, prob=T)
+curve(dexp(x, 1/8), add=T, col="red")
+
+# Problem 48
+1 - pnorm(240, 260, 50)
+pnorm(300, 260, 50) - pnorm(180, 260, 50)
+qnorm(0.1, 260, 50)
+
 # Problem 49
 library(MASS)
 data(survey)
@@ -66,6 +84,33 @@ table(age.interval)
 barplot(table(age.interval))
 hist(Age)
 stripchart(Age, method="stack", pch="*")
+
+# Problem 52
+v1 <- rep(4, 30)
+v2 <- rep(c(3.5,4.5), 15)
+v3 <- rep(c(3,5), 15)
+v4 <- rep(c(2:6), 6)
+v5 <- rep(c(2,6), 15)
+
+hist(v1)
+mean(v1)
+sd(v1)
+
+hist(v2)
+mean(v2)
+sd(v2)
+
+hist(v3)
+mean(v3)
+sd(v3)
+
+hist(v4)
+mean(v4)
+sd(v4)
+
+hist(v5)
+mean(v5)
+sd(v5)
 
 # Problem 53
 getwd()
@@ -110,3 +155,30 @@ my.summary(Pulse[Sex=="Female"])
 my.summary(Pulse[Age <= 25])
 my.summary(Pulse[Exer=="Freq"])
 my.summary(Pulse[Exer=="Freq" & Smoke=="Never"])
+
+# Problem 56
+p_freq <- mean(Pulse[Exer=="Freq"], na.rm=T)
+p_none <- mean(Pulse[Exer=="None"], na.rm=T)
+p_some <- mean(Pulse[Exer=="Some"], na.rm=T)
+
+barplot(c(p_none, p_some, p_freq), names.arg = c("None", "Some", "Freq"))
+
+# Problem 57
+plot(Smoke)
+
+# Problem 58
+my.summary <- function(x) {
+  res <- c(median(x, na.rm=T), mean(x, na.rm=T), sd(x, na.rm=T))
+  names(res) <- c("Median", "Mean", "Standart deviation")
+  res
+}
+
+my.summary(Age)
+my.summary(Age[Smoke!="Never"])
+my.summary(Age[W.Hnd=="Right"])
+my.summary(Age[Pulse >= 70])
+my.summary(Age[Exer=="None"])
+
+# Problem 59
+barplot(table(cut(Height, breaks=(seq(140, 200, 5)))))
+barplot(table(Sex, cut(Height, breaks=(seq(140, 200, 10)))), beside=T, legend.text=T)
