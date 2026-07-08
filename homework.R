d45beba320b25c4a34029be7141efcd5a37233b6
Viktor Pazhev <asusvikp@gmail.com>
2026-07-08 11:15:14 +0300

    Add homework problem 3
    Introduce frogWalk() to simulate a frog moving on a pentagon until all positions are visited. Run Monte Carlo replicates (1e3, 1e4, 1e5) and compute means, then plot a histogram of the final replicate results.

--- homework.R
+++ homework.R
@@ -1,3 +1,42 @@
+# Problem 3
+
+frogWalk <- function() {
+  steps <- 0
+  curr <- 1
+  moves <- c(-1, 1)
+  visited <- c(T, F, F, F, F)
+  
+  while (any(!visited)) {
+    move <- sample(moves, 1)
+    curr <- curr + move
+    
+    if (curr == 0) {
+      curr <- 5
+    }
+    
+    if (curr == 6) {
+      curr <- 1
+    }
+    
+    visited[curr] <- T
+    steps <- steps + 1
+  }
+  
+  steps
+}
+
+result <- replicate(10^3, frogWalk())
+mean(result)
+
+result <- replicate(10^4, frogWalk())
+mean(result)
+
+result <- replicate(10^5, frogWalk())
+mean(result)
+
+hist(result)
+
+# Problem 7
 fx <- function (x) {
   2*exp(-x^2/2)/sqrt(2*pi)
 }
